a87a75441254d6c74cb634ed98600363f20c347d
SmirnovGrigory <63301523+SmirnovGrigory@users.noreply.github.com>
2020-11-19 21:57:43 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -3,9 +3,9 @@
 
 double calcDelta()
 {
-	Circle a = Circle(6378.1);
+	Circle a = Circle(6378100);
 	Circle b = Circle(1);
-	b.setFerence(a.getFerence() + 0.001);
+	b.setFerence(a.getFerence() + 1);
 	return b.getRadius() - a.getRadius();
 }
 
@@ -14,6 +14,6 @@ double calcCost()
 	Circle pool(3);
 	Circle road(4);
 	double Sroad = road.getArea() - pool.getArea();
-	double fence = pool.getFerence();
+	double fence = road.getFerence();
 	return fence * 2000 + Sroad * 1000;
 }
